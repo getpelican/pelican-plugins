6334a890f431030b77c5413dc1d3fe23109d1a8e
Emmet McPoland <emcpoland@gmail.com>
2013-05-05 01:42:54 +0100

    Corrected indentation

--- pelicanext/thumbnailer/thumbnailer.py
+++ pelicanext/thumbnailer/thumbnailer.py
@@ -6,35 +6,35 @@ from pilkit.processors import *
 def thumbnailer(generator):
 
     resize = generator.settings['RESIZE']
-    
+
     outputpath = generator.settings['OUTPUT_PATH']
     imagesoutputpath = os.path.join(outputpath,'static', 'images')
-    
-    
+
+
     for path, suffix, processes in resize:
-    
-    	processor = ProcessorPipeline(processes)
-    	
-    	processpath = os.path.join(imagesoutputpath, path)
-    	
+
+        processor = ProcessorPipeline(processes)
+
+        processpath = os.path.join(imagesoutputpath, path)
+
         for dirpath, dirnames, filenames in os.walk(processpath):
             for name in filenames:
                 imagepath = os.path.join(dirpath, name)
-                
+
                 #Find output path
                 relpath = os.path.join(os.path.relpath(dirpath,processpath), name)
                 thumbnailpath = os.path.join(imagesoutputpath, path+suffix)
                 outputimagepath = os.path.join(thumbnailpath,relpath)
-                
+
                 #Create output directory if it doesnt exist
                 (dpath,dfile) = os.path.split(outputimagepath)
-        	if not os.path.isdir(dpath):
-        	    os.makedirs(dpath)
-        	    
-                #Process image                
+                if not os.path.isdir(dpath):
+                    os.makedirs(dpath)
+
+                #Process image
                 image = Image.open(imagepath)
                 image = processor.process(image)
-                
+
                 image.save(outputimagepath)
 
 
