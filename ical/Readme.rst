2b121e494eefc5983cf31b399419bb252cbbf2e0
cozo <julien.ortet@gmail.com>
2013-05-19 14:59:43 +0200

    Add multi calendar, one different calendar by page can be display

--- ical/Readme.rst
+++ ical/Readme.rst
@@ -5,11 +5,14 @@ This plugin read the calendar defined in the page metadata :calendar:
 
 Example of code that can be added in page template ::
 
+
 	{% if page.calendar %}
 	<dl>
-		{% for vevent in iCal %}
+		{% for vevent in  events[page.slug] %}
 			<dt>{{ vevent.summary }}</dt>
 			<dd>{{ vevent.description }}</dd>
+			<dd>{{ vevent.dtstart }}</dd>
+			<dd>{{ vevent.dtend }}</dd>
 		{% endfor %}
     </dl>
     {% endif %}
--- ical/ical.py
+++ ical/ical.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-iCal plugin for Pelican
+ical plugin for Pelican
 ===========================
 
-descritpion
+This plugin read the calendar defined in the page metadata :calendar:
+
+
 """
 
 from icalendar import Calendar, Event
 from pelican import signals
+from pelican import utils
 import pytz
 import datetime
 
+def init_cal(generator):
+	calDict = {}
+	generator.context['events'] = calDict
+
 def add_ical(generator, metadata):
-	"""Provides events list to templates
-	"""
-	
 	if 'calendar' in metadata.keys():
 		summ = []
 		path = metadata['calendar']
@@ -23,11 +27,15 @@ def add_ical(generator, metadata):
 			eventdict = {}
 			if element.name == "VEVENT":
 				eventdict['summary'] = element.get('summary')
-				eventdict['description'] = element.get('description').replace('\n\n', '<br>')
+				if element.get('description') != None :
+					eventdict['description'] = element.get('description').replace('\n\n', '<br>')
 				eventdict['dtstart'] = element.get('dtstart').dt
 				eventdict['dtend'] = element.get('dtend').dt
 				summ.append(eventdict)
-		generator.context['events'] = summ
+		calId = utils.slugify(metadata['title'])
+		generator.context['events'][calId] = summ
+
 		
 def register():
+    signals.pages_generator_init.connect(init_cal)
     signals.pages_generate_context.connect(add_ical)
