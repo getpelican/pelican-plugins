b853f8efcf4d88d61aec15a2aad75229e543986a
eike2013 <git@x4343.org>
2013-07-12 12:48:48 +0200

    Update better_figures_and_images.py
    Change the path extraction method.

--- better_figures_and_images/better_figures_and_images.py
+++ better_figures_and_images/better_figures_and_images.py
@@ -31,7 +31,14 @@ def content_object_init(instance):
             for img in soup('img'):
                 # TODO: Pretty sure this isn't the right way to do this, too hard coded.
                 # There must be a setting that I should be using?
-                src = instance.settings['PATH'] + '/images/' + os.path.split(img['src'])[1]
+                #src = instance.settings['PATH'] + '/images/' + os.path.split(img['src'])[1]
+                # The method mentioned above is only working if the images are really in the "images" folder.
+                # It's also not working on subdirectories inside the image folder
+                # Both issues are fixed:
+                # Changed the static "/images/" string to the proper path which is extracted from the 'split' tuple
+                # The first 7 letters are cutted ("/static") to get a valid link.
+                # Somehow the static folder isn't created in the output folder. It's only on the server after 'make ftp_upload'
+                src = instance.settings['PATH'] + os.path.split(img['src'])[0][7:] +'/' + os.path.split(img['src'])[1]
                 im = Image.open(src)
                 extra_style = 'width: {}px; height: auto;'.format(im.size[0])
 
