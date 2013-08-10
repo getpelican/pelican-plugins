e20043dd1426eba5cb24d1cd75b892ad5ef037b4
Joey Coleman <jwc@iha.dk>
2013-08-10 21:27:17 +0200

    Cleanup (very quick) the reader code, add a few comments

--- textile_reader/textile_reader.py
+++ textile_reader/textile_reader.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-import datetime
-import logging
-import os
-import re
-
 from pelican import signals
 from pelican.readers import BaseReader
-from pelican.contents import Page, Category, Tag, Author
-from pelican.utils import get_date, pelican_open
-
+from pelican.utils import pelican_open
 
 try:
     from textile import textile
 except ImportError:
     textile = False
 
-
 class TextileReader(BaseReader):
+    """Reader for Textile files.  Written using the core MarkdownReader as
+a template.  Textile input files must be of the form:
+
+Title: An example
+Date: 2013-08-10
+----
+p. Lorem ipsum dolar sit amet...
+
+Specifically, the header values as with Markdown files, then four
+dashes, then the body.
+
+    """
+
     enabled = bool(textile)
     file_extensions = ['textile']
 
@@ -26,26 +31,28 @@ class TextileReader(BaseReader):
         super(TextileReader, self).__init__(*args, **kwargs)
 
     def _parse_metadata(self, meta):
-        """Return the dict containing document metadata"""
+        """Process the metadata dict, lowercasing the keys and textilizing the
+value of the 'summary' key (if present).  Keys that share the same
+lowercased form will be overridden in some arbitrary order.
+
+        """
         output = {}
         for name, value in meta.items():
             name = name.lower()
             if name == "summary":
-                summary = textile(value)
-                output[name] = self.process_metadata(name, summary)
-            else:
-                output[name] = self.process_metadata(name, value)
+                value = textile(value)
+            output[name] = self.process_metadata(name, value)
         return output
 
     def read(self, source_path):
-        """Parse content and metadata of textile files"""
+        """Parse content and metadata of textile files."""
 
         with pelican_open(source_path) as text:
             parts = text.split('----',1)
             if len(parts)==2:
                 headerlines = parts[0].splitlines()
                 headerpairs = map(lambda l:l.split(':',1), headerlines)
-                headerdict = { pair[0]:pair[1].strip()
+                headerdict = { pair[0] : pair[1].strip()
                                for pair in headerpairs
                                if len(pair)==2 }
                 metadata = self._parse_metadata(headerdict)
