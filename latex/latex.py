768e02b2d514a699b39e9f49f1ca6e34d60f704b
zhouji <calf.zhou@gmail.com>
2013-09-27 16:28:46 +0800

    Update signal names for Pelican 3.2+

--- latex/latex.py
+++ latex/latex.py
@@ -45,4 +45,4 @@ def register():
         Plugin registration
     """
     signals.article_generator_context.connect(addLatex)
-    signals.pages_generate_context.connect(addLatex)
+    signals.page_generator_context.connect(addLatex)
