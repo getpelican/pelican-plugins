2b373628738c9f053a8574f2080ca4379e55b7d0
Khue Vu <khuevu@gmail.com>
2014-05-06 00:27:42 +0800

    add the plugin to extract representative image for article and assign to an article's property

---
+++ representative_image/Readme.md
@@ -0,0 +1,8 @@
+# Summary
+
+This plugin extracts a presentative image from the article's summary or article's content. The image can be access at `article.repImage`. 
+
+The plugin also remove any images from the summary after extraction to avoid duplication. 
+
+It allows the flexibility on where and how to display the representative image of an article together with its summary in a template page. For example, the article metadata can be displayed in thumbnail format, in which there is a short summary and an image. The layout of the summary and the image can be varied for aesthetical purpose. It doesn't have to depend on article's content format. 
+
---
+++ representative_image/representative_image.py
@@ -0,0 +1,31 @@
+from pelican import signals
+from pelican.contents import Content, Article
+from bs4 import BeautifulSoup
+
+def images_extraction(instance):
+    representativeImage = None
+    if type(instance) == Article:
+        # Process Summary:
+        # If summary contains images, extract one to be the representativeImage and remove images from summary
+        soup = BeautifulSoup(instance.summary, 'html.parser')
+        images = soup.find_all('img')
+        for i in images:
+            if not representativeImage:
+                representativeImage = i['src']
+            i.extract()
+        if len(images) > 0:
+            # set _summary field which is based on metadata. summary field is only based on article's content and not settable
+            instance._summary = unicode(soup)
+        
+        # If there are no image in summary, look for it in the content body
+        if not representativeImage:
+            soup = BeautifulSoup(instance.content, 'html.parser')
+            imageTag = soup.find('img')
+            if imageTag:
+                representativeImage = imageTag['src']
+        
+        # Set the attribute to content instance
+        instance.repImage = representativeImage
+
+def register():
+    signals.content_object_init.connect(images_extraction)
