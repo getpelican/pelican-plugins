a3127d6599aba759007ba3f7a506c082541a5a40
zhangliyong <lyzhang87@gmail.com>
2014-09-23 10:42:41 +0700

    FIX: Sitemap offset-naive datetimes(modified) error

--- sitemap/sitemap.py
+++ sitemap/sitemap.py
@@ -180,7 +180,7 @@ class SitemapGenerator(object):
             for article in articles:
                 lastmod = max(lastmod, article.date.replace(tzinfo=self.timezone))
                 try:
-                    modified = self.get_date_modified(article, datetime.min.replace(tzinfo=self.timezone));
+                    modified = self.get_date_modified(article, datetime.min).replace(tzinfo=self.timezone)
                     lastmod = max(lastmod, modified)
                 except ValueError:
                     # Supressed: user will be notified.
