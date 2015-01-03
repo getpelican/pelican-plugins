b0e6993e8a9dae8353c811c4a4c474cb37665119
Giel van Schijndel <me@mortis.eu>
2015-01-03 22:24:53 +0100

    share_post: add share-to-Diaspora* support
    This uses the sharetodiaspora.github.io HTML5 posting app, necessary due
    to Diaspora*'s multi-homed nature.

--- share_post/share_post.py
+++ share_post/share_post.py
@@ -39,12 +39,15 @@ def share_post(content):
     summary = article_summary(content)
 
     tweet = ('%s%s%s' % (title, quote(' '), url)).encode('utf-8')
+    diaspora_link = 'https://sharetodiaspora.github.io/?title=%s&url=%s' % (title, url)
     facebook_link = 'http://www.facebook.com/sharer/sharer.php?s=100&amp;p%%5Burl%%5D=%s' % url
     gplus_link = 'https://plus.google.com/share?url=%s' % url
     twitter_link = 'http://twitter.com/home?status=%s' % tweet
     mail_link = 'mailto:?subject=%s&amp;body=%s' % (title, url)
 
-    share_links = {'twitter': twitter_link,
+    share_links = {
+                   'diaspora': diaspora_link,
+                   'twitter': twitter_link,
                    'facebook': facebook_link,
                    'google-plus': gplus_link,
                    'email': mail_link
