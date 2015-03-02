4bd97b0fca7e4d541d038b1b611e4e6001ad9bc2
Sergej Chodarev <sergej.chodarev@ya.ru>
2015-03-02 17:59:55 +0100

    Reorganize feed_template plugin code and improve documentation

--- feed_template/feed_template.py
+++ feed_template/feed_template.py
@@ -1,3 +1,6 @@
+"""
+Feed Template Pelican plugin allows to customize article title, link and description in RSS feeds using a template.
+"""
 from pelican import signals
 from pelican.writers import Writer
 import six
@@ -6,42 +9,39 @@ from six.moves.urllib.parse import urlparse
 from pelican.utils import set_date_tzinfo
 
 
-macros = None
-
-
 def load_template(generator):
+    """Load 'feed' template and store its macros in FeedCustomizationWriter."""
     try:
-        global macros
-        macros = generator.get_template("feed").make_module(generator.context)
+        tpl_macros = generator.get_template("feed").make_module(generator.context)
+        FeedCustomizationWriter.macros = tpl_macros
     except Exception:
         pass
 
 
 def get_writer(pelican_object):
-    if macros is not None:
-        return FeedCustomizationWriter
-    else:
-        return None
+    """Provide customuzed writer."""
+    return FeedCustomizationWriter
 
 
 class FeedCustomizationWriter(Writer):
-    def _add_item_to_the_feed(self, feed, item):
-        """
-        Custom function for adding articles to the feed.
+    """Writer that allows to customize feeds using template macros."""
+    macros = None  # Template macros
 
-        Can replaces original one from the pelican.writers.Writer class
-        and adds processing article data using template macros.
-        """
+    def _add_item_to_the_feed(self, feed, item):
+        if self.macros is None:
+            # Fall back to default behaviour if macros was not loaded
+            return Writer._add_item_to_the_feed(self, feed, item)
         title = Markup(item.title).striptags()
         link = '%s/%s' % (self.site_url, item.url)
         content = item.get_content(self.site_url)
         feed.add_item(
-            title=macros.title(item) if hasattr(macros, 'title') else title,
-            link=macros.link(item) if hasattr(macros, 'link') else link,
+            title=self.macros.title(item) if hasattr(self.macros, 'title') else title,
+            link=self.macros.link(item) if hasattr(self.macros, 'link') else link,
             unique_id='tag:%s,%s:%s' % (urlparse(link).netloc,
                                         item.date.date(),
                                         urlparse(link).path.lstrip('/')),
-            description=macros.description(item) if hasattr(macros, 'description') else content,
+            description=self.macros.description(item) if hasattr(self.macros, 'description') \
+                                                      else content,
             categories=item.tags if hasattr(item, 'tags') else None,
             author_name=getattr(item, 'author', ''),
             pubdate=set_date_tzinfo(
