9549833933439949e4446997fa055a452517bda0
St John Karp <git@fuzzjunket.com>
2015-09-07 19:20:36 -0700

    Convert the Hubski Static Comments plugin to use the new API
    Converted the Hubski Static Comments plugin from screen-scraping
    the website to using the prototype API. For details of the API,
    see the following post:
    https://hubski.com/pub?id=266825

--- hubski_static/hubski_static.py
+++ hubski_static/hubski_static.py
@@ -3,66 +3,77 @@
 Static Discussion via Hubski plugin for Pelican
 ====================================
 This plugin makes the following properties available to an article:
-- hubski_comments: contains the HTML of all comments screen-scraped
-  from Hubski.
+- hubski_comments: contains the all child publications (i.e. comments) fetched from the Hubski API.
 - hubski_comment_count: contains the number of comments
-- hubski_post_url: the URL of this post on Hubski
 
 The plugin cannot search Hubski for your post. Each post must
 have a "Hubski_ID" property in the metadata which contains
 the ID of the post on Hubski. Posts without an ID will be
 skipped.
 
-By default this plugin uses "https://hubski.com" as the Hubski URL,
-and appends "/pub?id=" as the resource. If these change, the default
-values can be overridden in the settings using HUBSKI_URL and
-HUBSKI_POST_URL.
+This plugin gets all its Hubski URLs from the Pelican conf file so that
+they can easily be updated if the API changes. The following are the
+valid URLs at the time of writing:
+HUBSKI_URL = 'https://hubski.com'
+HUBSKI_POST_URL = HUBSKI_URL + '/pub?id='
+HUBSKI_USER_URL = HUBSKI_URL + '/user?id='
+HUBSKI_API_TREE_URL = HUBSKI_URL + '/api/publication/$id/tree'
 """
 
 from __future__ import unicode_literals
 from bs4 import BeautifulSoup
+from datetime import date
 from pelican import signals
 import requests
+from string import Template
 
 def hubski_static(generator):
-    # Get settings -- the defaults should work, but can be overridden
-    # if Hubski changes its URL format
-    hubski_url = generator.settings.get('HUBSKI_URL', 'https://hubski.com')
-    hubski_post_url = generator.settings.get('HUBSKI_POST_URL', hubski_url + '/pub?id=')
+    # API end-points should be defined in the conf file so they can easily be changed
+    hubski_url = generator.settings.get('HUBSKI_URL')
+    tree_template = Template(generator.settings.get('HUBSKI_API_TREE_URL'))
 
     for article in generator.articles:
         post_id = getattr(article, 'hubski_id', None)
-        if post_id:
-            post = requests.get(hubski_post_url+post_id)
-            soup = BeautifulSoup(post.text)
+        if post_id is not None:
+            post_tree_url = tree_template.safe_substitute(id=post_id)
+            publication = requests.get(post_tree_url).json()
 
-            # Get the smallest div we can that contains all the comments
-            comments = soup.find('div', {'class': 'rightmenu'}).next_sibling
+            # Perform any desired transformations on the comments
+            comments, comment_count = hubski_alter_comments(hubski_url, publication)
 
-            # Scrape away the bits that aren't comments
-            comments.find('div', {'class': 'sub'}).extract()
-            comments.find('div', {'class': 'wholepub'}).extract()
+            # Make a count of top-level comments available in the templates
+            article.hubski_comment_count = comment_count
 
-            # Remove reply links and voting divs
-            for links in comments.find_all('div', {'class': ['plusminus', 'underlink']}):
-                links.extract()
+            # Make the comments available to the templates
+            article.hubski_comments = comments
 
-            # Update relative links with full paths
-            # Also make them open in a new window
-            for a in comments.find_all('a'):
+def hubski_alter_comments(hubski_url, publication):
+    children = []
+    children_count = 0
+    if 'children' in publication:
+        for child in publication['children']:
+            soup = BeautifulSoup(child['text'])
+            for a in soup.find_all('a'):
+                # Update relative links with full paths
                 if not a['href'].startswith('http'):
                     a['href'] = hubski_url + '/' + a['href']
+                # Also make them open in a new window
                 a['target'] = '_blank'
+            child['text'] = unicode(soup)
 
-            # Make a count of comments available in the templates
-            article.hubski_comment_count = len(comments.find_all('div', {'class': 'outercomm'}))
+            # Human-readable date
+            child['iso_date'] = date.fromtimestamp(child['time']).isoformat()
 
-            # Convert the soup object to a string
-            # and make it available to the templates
-            article.hubski_comments = unicode(comments)
+            # Recursively process the child's children
+            child['children'], sub_children_count = hubski_alter_comments(hubski_url, child)
 
-            # Create a link to this post on Hubski
-            article.hubski_post_url = hubski_post_url + post_id
+            # Keep a running total of all children
+            children_count = children_count + sub_children_count
+
+            children.append(child)
+
+    children_count = children_count + len(children)
+    return [children, children_count]
 
 def register():
     signals.article_generator_finalized.connect(hubski_static)
