5c53c90a5b48360ad17beeeadd2e4e207673d63f
howthebodyworks <git@email.possumpalace.org>
2017-01-03 19:33:02 +0100

    whitespace errors

--- render_math/pelican_mathjax_markdown_extension.py
+++ render_math/pelican_mathjax_markdown_extension.py
@@ -53,7 +53,7 @@ class PelicanMathJaxCorrectDisplayMath(markdown.treeprocessors.Treeprocessor):
             el.text = text
             el.extend(children[current_idx:idx])
 
-            # Test to ensure that empty <p> is not inserted  
+            # Test to ensure that empty <p> is not inserted
             if len(el) != 0 or (el.text and not el.text.isspace()):
                root.insert(insert_idx, el)
                insert_idx += 1
@@ -90,7 +90,7 @@ class PelicanMathJaxCorrectDisplayMath(markdown.treeprocessors.Treeprocessor):
                 continue
 
             insert_idx = list(root).index(parent)
-            self.correct_html(root, children, div_math, insert_idx, parent.text) 
+            self.correct_html(root, children, div_math, insert_idx, parent.text)
             root.remove(parent)  # Parent must be removed last for correct insertion index
 
         return root
@@ -145,7 +145,7 @@ class PelicanMathJaxExtension(markdown.Extension):
         md.inlinePatterns.add('mathjax_displayed', PelicanMathJaxPattern(self, 'div', mathjax_display_regex), '<escape')
         md.inlinePatterns.add('mathjax_inlined', PelicanMathJaxPattern(self, 'span', mathjax_inline_regex), '<escape')
 
-        # Correct the invalid HTML that results from teh displayed math (<div> tag within a <p> tag) 
+        # Correct the invalid HTML that results from teh displayed math (<div> tag within a <p> tag)
         md.treeprocessors.add('mathjax_correctdisplayedmath', PelicanMathJaxCorrectDisplayMath(self), '>inline')
 
         # If necessary, add the JavaScript Mathjax library to the document. This must
