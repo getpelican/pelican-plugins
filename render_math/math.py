952c8506f71cd6fba9a65d18dbbbd5361b9b4a8c
Dan MacKinlay <myopenid@email.possumpalace.org>
2017-05-01 14:54:31 +1000

    [render_math] typo in javascript string

--- render_math/math.py
+++ render_math/math.py
@@ -126,7 +126,7 @@ def process_settings(pelicanobj):
     # Source for MathJax
     mathjax_settings['source'] = (
         "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js"
-        "?config=TeX-AMS-MML_HTMLorMML'"
+        "?config=TeX-AMS-MML_HTMLorMML"
     )
 
     # Get the user specified settings
