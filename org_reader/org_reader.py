c15a07fc0054de1e9152e00a66798f69329ab357
Gendre Sébastien <s.gendre@openmailbox.org>
2017-10-19 08:54:02 +0200

    Create org_reader.py, with imports and org_reader class declaration
    Empty method for now, just declaration

---
+++ org_reader/org_reader.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2017  Sébastien Gendre
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import re
+from orgpython import org_to_html
+from pelican import signals
+from pelican.readers import BaseReader
+from pelican.utils import pelican_open
+
+class OrgReader(BaseReader):
+    """Reader for Org files"""
+    enabled = True
+    file_extensions = ['org']
+    
