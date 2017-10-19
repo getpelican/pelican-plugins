b167d70e428e4449db75aacd54a1db6d2dcd29c3
Gendre Sébastien <s.gendre@openmailbox.org>
2017-10-19 11:38:07 +0200

    Org_reader: Remove a lost 'y' char in a file

--- org_reader/__init__.py
+++ org_reader/__init__.py
@@ -1,4 +1,4 @@
-y# Copyright (C) 2017  Sébastien Gendre
+# Copyright (C) 2017  Sébastien Gendre
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
