e0c51580af45e29c0e413603f07b6405d82eadf0
Oliver Urs Lenz <oliver.urs.lenz@gmail.com>
2018-02-27 18:09:11 +0100

    categories as top-level subcategories, make subcategories available in context

--- subcategory/README.md
+++ subcategory/README.md
@@ -2,8 +2,8 @@
 
 This plugin adds support for subcategories in addition to article categories.
 
-Subcategories are hierarchical. Each subcategory has a parent, which is either a
-regular category or another subcategory.
+Subcategories are hierarchical. Top-level subcategories correspond to categories,
+all other subcategories have another subcategory as their parent.
 
 Feeds can be generated for each subcategory, just like categories and tags.
 
@@ -25,9 +25,6 @@ breadcrumb-style navigation you might try something like this:
 
     <nav class="breadcrumb">
     <ol>
-        <li>
-            <a href="{{ SITEURL }}/{{ article.category.url }}">{{ article.category}}</a>
-        </li>
     {% for subcategory in article.subcategories %}
         <li>
             <a href="{{ SITEURL }}/{{ subcategory.url }}">{{ subcategory.shortname }}</a>
@@ -43,11 +40,16 @@ to extract the article path (containing all category and subcategory folders)
 into the `subcategory_path` metadata. The following settings would use all available 
 subcategories for the hierarchy:
 
-    PATH_METADATA= '(?P<subcategory_path>.*)/.*'
+    PATH_METADATA = '(?P<subcategory_path>.*)/.*'
+
+If you store all articles in a single `articles/` folder that you want to ignore for the purpose of subcategories, use:
+
+    PATH_METADATA = 'articles/(?P<subcategory_path>.*)/.*'
 
 You can limit the depth of generated subcategories by adjusting the regular expression
 to only include a specific number of path separators (`/`). For example, the following 
-would generate only a single level of subcategories regardless of the folder tree depth:
+would generate only a single level of subcategories under the category level,
+regardless of the folder tree depth:
 
     PATH_METADATA= '(?P<subcategory_path>[^/]*/[^/]*)/.*'
 
@@ -64,8 +66,10 @@ its parents associated. For example if you had…
 … the full name for Sub Category2 would be `Category/Sub Category1/Sub Category2` and
 the "short name" would be `Sub Category2`.
 
-If you need to use the slug, it is generated from the short name — not the full
-name.
+If you need to use the slug, it is generated recursively from the full name by slugifying the shortname of the
+subcategory and its parents:
+
+    slugified-category/slugified-sub-category/slugified-sub-sub-category
 
 ##Settings##
 
@@ -73,14 +77,9 @@ Consistent with the default settings for Tags and Categories, the default
 settings for subcategories are:
 
     'SUBCATEGORY_SAVE_AS' = os.path.join('subcategory', '{savepath}.html')
-    'SUBCATEGORY_URL' = 'subcategory/(fullurl).html'
+    'SUBCATEGORY_URL' = 'subcategory/(slug).html'
 
-`savepath` and `fullurl` are generated recursively, using slugs. So the full
-URL would be:
-
-    category-slug/sub-category-slug/sub-sub-category-slug
-
-… with `savepath` being similar but joined using `os.path.join`.
+`savepath` is identical to the slug, except that `savepath` is joined using `os.path.join`.
 
 Similarly, you can save subcategory feeds by adding one of the following
 to your Pelican configuration file:
@@ -88,14 +87,28 @@ to your Pelican configuration file:
     SUBCATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
     SUBCATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
-… and this will create a feed with `fullurl` of the subcategory. For example:
+… and this will create a feed with `slug` of the subcategory. For example:
 
     feeds/category/subcategory.atom.xml
 
 Article urls can also use the values of `subpath` and `suburl` in their
-definitions. These are equivalent to the `fullurl` and `savepath` of the most
-specific subcategory. If you have articles that don't have subcategories these
-values are set to the category slug.
+definitions. These are equivalent to the `savepath` and `slug` of the most
+specific subcategory.
 
-    ARTICLE_SAVE_AS = os.path.join('{subpath}' 'articles' '{slug}.html')
+    ARTICLE_SAVE_AS = os.path.join('{subpath}', 'articles', '{slug}.html')
     ARTICLE_URL = '{suburl}/articles/{slug}.html'
+
+##Subcategories in templates##
+The list `subcategories` of all pairs of subcategories with their corresponding articles is available in the context
+analogous to `categories`, and can be used in templates, e.g. to make a menu of available subcategories.
+This list is ordered lexicographically, so subcategories always follow their parent:
+
+    aba
+    aba/dat
+    abaala
+
+The order may be reversed by setting:
+
+    REVERSE_SUBCATEGORY_ORDER = False
+
+… to `True`, analogous to `REVERSE_CATEGORY_ORDER`.
--- subcategory/subcategory.py
+++ subcategory/subcategory.py
@@ -5,8 +5,6 @@
 Adds support for subcategories on pelican articles
 """
 import os
-from collections import defaultdict
-from operator import attrgetter
 from functools import partial
 
 from pelican import signals
@@ -21,49 +19,40 @@ class SubCategory(URLWrapper):
         self.parent = parent
         self.shortname = name.split('/')
         self.shortname = self.shortname.pop()
-        self.slug = slugify(self.shortname, settings.get('SLUG_SUBSTITUIONS', ()))
-        if isinstance(self.parent, SubCategory):
-            self.savepath = os.path.join(self.parent.savepath, self.slug)
-            self.fullurl = '{}/{}'.format(self.parent.fullurl, self.slug)
-        else: #parent is a category
-            self.savepath = os.path.join(self.parent.slug, self.slug)
-            self.fullurl = '{}/{}'.format(self.parent.slug, self.slug)
+        self.slug = slugify(self.shortname, settings.get('SLUG_SUBSTITUTIONS', ()))
+        if self.parent:
+            self.slug = '{}/{}'.format(self.parent.slug, self.slug)
+        self.savepath = os.path.join(*self.slug.split('/'))
         
     def as_dict(self):
-        d = self.__dict__
+        d = super(SubCategory, self).as_dict()
         d['shortname'] = self.shortname
         d['savepath'] = self.savepath
-        d['fullurl'] = self.fullurl
         d['parent'] = self.parent
         return d
-
-    def __hash__(self):
-        return hash(self.fullurl)
-
-    def _key(self):
-        return self.fullurl
 
 def get_subcategories(generator, metadata):
     if 'SUBCATEGORY_SAVE_AS' not in generator.settings:
         generator.settings['SUBCATEGORY_SAVE_AS'] = os.path.join( 
                 'subcategory', '{savepath}.html')
     if 'SUBCATEGORY_URL' not in generator.settings:
-        generator.settings['SUBCATEGORY_URL'] = 'subcategory/{fullurl}.html'
+        generator.settings['SUBCATEGORY_URL'] = 'subcategory/{slug}.html'
 
     if 'subcategory_path' in metadata:
         category_list = text_type(metadata.get('subcategory_path')).split('/')
     else:
         category_list = text_type(metadata.get('category')).split('/')
 
-    category = (category_list.pop(0)).strip()
+    category = category_list[0].strip()
     category = Category(category, generator.settings)
     metadata['category'] = category
     #generate a list of subcategories with their parents
     sub_list = []
-    parent = category.name
+    parent = None
     for subcategory in category_list:
         subcategory.strip()
-        subcategory = parent + '/' + subcategory
+        if parent:
+            subcategory = parent + '/' + subcategory
         sub_list.append(subcategory)
         parent = subcategory
     metadata['subcategories'] = sub_list
@@ -71,12 +60,11 @@ def get_subcategories(generator, metadata):
 def create_subcategories(generator):
     generator.subcategories = []
     for article in generator.articles:
-        parent = article.category
+        parent = None
         actual_subcategories = []
         for subcategory in article.subcategories:
             #following line returns a list of items, tuples in this case
-            sub_cat = [item for item in generator.subcategories 
-                    if item[0].name == subcategory]
+            sub_cat = [item for item in generator.subcategories if item[0].name == subcategory]
             if sub_cat:
                 sub_cat[0][1].append(article)
                 parent = sub_cat[0][0]
@@ -88,24 +76,21 @@ def create_subcategories(generator):
                 actual_subcategories.append(parent)
         article.subcategories = actual_subcategories
         """Add subpath and suburl to the article metadata. This allows the
-        the last subcategory's fullurl and savepath to be used when definining
-        Article URL's. If an article has no subcategories, the Category slug
-        is used instead
+        last subcategory's slug and savepath to be used when defining
+        the article URL.
         """
-        try:
-            last_subcat = article.subcategories[-1]
-            article.metadata['subpath'] = last_subcat.savepath
-            article.metadata['suburl'] = last_subcat.fullurl
-        except IndexError: #No Subcategory
-            article.metadata['subpath'] = article.category.slug
-            article.metadata['suburl'] = article.category.slug
+        last_subcat = article.subcategories[-1]
+        article.metadata['subpath'] = last_subcat.savepath
+        article.metadata['suburl'] = last_subcat.slug
+
+    generator.subcategories.sort(reverse=generator.settings.get('REVERSE_SUBCATEGORY_ORDER') or False)
+    generator._update_context(['subcategories'])
 
 def generate_subcategories(generator, writer):
     write = partial(writer.write_file,
             relative_urls=generator.settings['RELATIVE_URLS'])
     subcategory_template = generator.get_template('subcategory')
     for subcat, articles in generator.subcategories:
-        articles.sort(key=attrgetter('date'), reverse=True)
         dates = [article for article in generator.dates if article in articles]
         write(subcat.save_as, subcategory_template, generator.context, 
                 subcategory=subcat, articles=articles, dates=dates, 
@@ -114,15 +99,14 @@ def generate_subcategories(generator, writer):
 
 def generate_subcategory_feeds(generator, writer):
     for subcat, articles in generator.subcategories:
-        articles.sort(key=attrgetter('date'), reverse=True)
         if generator.settings.get('SUBCATEGORY_FEED_ATOM'):
             writer.write_feed(articles, generator.context,
                     generator.settings['SUBCATEGORY_FEED_ATOM']
-                    % subcat.fullurl)
+                    % subcat.slug)
         if generator.settings.get('SUBCATEGORY_FEED_RSS'):
             writer.write_feed(articles, generator.context,
                     generator.settings['SUBCATEGORY_FEED_RSS']
-                    % subcat.fullurl, feed_type='rss')
+                    % subcat.slug, feed_type='rss')
 
 def generate(generator, writer):
     generate_subcategory_feeds(generator, writer)
