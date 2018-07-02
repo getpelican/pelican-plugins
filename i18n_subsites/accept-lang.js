323fd11372e157cc633afa972167ce28574e2d3c
Siltaar <siltaar@d12s.fr>
2018-07-02 18:55:56 +0200

    fix LibreJS compatible licence declaration

--- i18n_subsites/accept-lang.js
+++ i18n_subsites/accept-lang.js
@@ -1,7 +1,32 @@
-// name   : accept-lang.js
-// author : Simon Descarpentries, simon /\ acoeuro [] com
-// date   : 2018-01
-// licence: GPLv3
+/**
+ *
+ * @source: https://github.com/getpelican/pelican-plugins/raw/master/i18n_subsites/accept-lang.js
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2018  Simon Descarpentries
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
 //
 // jshint -W097
 //
