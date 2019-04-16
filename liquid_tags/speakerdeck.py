033af6f9351a5f3e7d5094594d481da0c26b76b6
Paolo Melchiorre <paolo@melchiorre.org>
2019-04-16 11:35:24 +0200

    Fix #1127
    Add an optional ratio for speakedeck viewer in Liquid tags plugin.

--- liquid_tags/speakerdeck.py
+++ liquid_tags/speakerdeck.py
@@ -13,23 +13,44 @@ Example
 
 Output
 ------
-<script async class="speakerdeck-embed" data-id="82b209c0f181013106da6eb14261a8ef"
- data-ratio="1.33333333333333" src="//speakerdeck.com/assets/embed.js"></script>
+<script
+    async
+    class="speakerdeck-embed"
+    data-id="82b209c0f181013106da6eb14261a8ef"
+    data-ratio="1.33333333333333"
+    src="//speakerdeck.com/assets/embed.js">
+</script>
 """
+import re
+
 from .mdx_liquid_tags import LiquidTags
 
-SYNTAX = "{% speakerdeck id %}"
+SYNTAX = "{% speakerdeck id [ratio] %}"
+
+REGEX = re.compile(r'([\S]+)(\s+(\d*\.?\d*))?')
+
 
 @LiquidTags.register('speakerdeck')
 def speakerdeck(preprocessor, tag, markup):
-    speakerdeck_out = """
+    ratio = 1.33333333333333
+    id = None
+    match = REGEX.search(markup)
+    if match:
+        groups = match.groups()
+        id = groups[0]
+        ratio = groups[2] or ratio
+    if id:
+        speakerdeck_out = """
 <script async class="speakerdeck-embed" data-id="{id}"
-data-ratio="1.33333333333333" src="//speakerdeck.com/assets/embed.js"></script>
-        """.format(id=markup)
-
+data-ratio="{ratio}" src="//speakerdeck.com/assets/embed.js"></script>
+        """.format(id=id, ratio=ratio)
+    else:
+        raise ValueError(
+            "Error processing input, expected syntax: {0}".format(SYNTAX)
+        )
     return speakerdeck_out
 
 
 # ---------------------------------------------------
 # This import allows speakerdeck tag to be a Pelican plugin
-from liquid_tags import register  # noqa
+from liquid_tags import register  # noqa # isort:skip
