cdc5f9f9b77243a29a0ed1f0f9e255d8bc887657
granitosaurus <tinarg@protonmail.com>
2019-05-27 08:58:12 +0000

    add docstring

--- shortcodes/shortcodes.py
+++ shortcodes/shortcodes.py
@@ -1,3 +1,27 @@
+"""
+shortcodes.py
+=============
+Copyright: GPLv3 <Bernardas Ališauskas @ bernardas.alisauskas@pm.me>
+
+This plugin allows to define macros called shortcodes in content pages that will be expanded as jinja2 templates.
+The purpose of this plugin is to allow explicit and quick jinja2 templating in your content without compromising markdown/rst.
+
+Example:
+
+    #pelicanconf.py
+
+    SHORTCODES = {
+        'image': "<img src=/images/{{src}}>{{desc|title}}<img>"
+    }
+
+Then in your content:
+
+    [% image src=foo.png desc="this is a test" %]
+
+will become:
+
+    <img src=/images/foo.png>This Is A Test<img>
+"""
 import re
 from typing import Match, Dict
 
