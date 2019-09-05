661922ef199aa55fb9262c006f7d43f2c743bfce
Lucas Cimon <lucas.cimon@gmail.com>
2019-09-05 09:18:05 +0200

    Fix: test_simple_footnotes.py

--- simple_footnotes/test_simple_footnotes.py
+++ simple_footnotes/test_simple_footnotes.py
@@ -18,11 +18,10 @@ class TestFootnotes(unittest.TestCase):
         self.assertEqual(art._content, expected_output)
 
     def test_simple(self):
-        self._expect("words[ref]footnote[/ref]end",
-        ('words<sup id="sf-article-1-back"><a title="footnote" '
-         'href="#sf-article-1" class="simple-footnote">1</a></sup>end'
-         '<ol class="simple-footnotes">'
-         u'<li id="sf-article-1">footnote <a href="#sf-article-1-back" class="simple-footnote-back">\u21a9</a></li>'
+        self._expect('words[ref]footnote[/ref]end',
+        ('words<sup id=sf-article-1-back><a href=#sf-article-1 class=simple-footnote title=footnote>1</a></sup>end'
+         '<ol class=simple-footnotes>'
+         u'<li id=sf-article-1>footnote <a href=#sf-article-1-back class=simple-footnote-back>\u21a9</a></li>'
          '</ol>'))
 
     def test_no_footnote_inside_code(self):
