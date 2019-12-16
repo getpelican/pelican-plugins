9b31101ac3749e82a73c3279c7712e0011cbc36b
dfoulks <dfoulks@apache.org>
2019-12-16 13:58:11 -0600

    PEP8 verified
    did PEP8 verification on the scripts via pycodestyle. they all pass.
    I also added a vars file so that we can set where the plugin will work.

--- pelican-gfm/gfm.py
+++ pelican-gfm/gfm.py
@@ -26,6 +26,7 @@ import ctypes
 import time
 import re
 import gfmSetup
+import gfmVars
 import pelican.utils
 import pelican.signals
 import pelican.readers
@@ -34,11 +35,13 @@ _LIBDIR = gfmSetup.LIBCMARKLOCATION
 _LIBCMARK = 'libcmark-gfm.so'
 _LIBEXT = 'libcmark-gfmextensions.so'
 try:
-      cmark = ctypes.CDLL(os.path.join(_LIBDIR, _LIBCMARK))
-      cmark_ext = ctypes.CDLL(os.path.join(_LIBDIR, _LIBEXT))
+    cmark = ctypes.CDLL(os.path.join(_LIBDIR, _LIBCMARK))
+    cmark_ext = ctypes.CDLL(os.path.join(_LIBDIR, _LIBEXT))
 except OSError:
-      raise ImportError('%s not found. see build-mark.sh and gfm_reader.py'
-                                  % _LIBCMARK)
+    raise ImportError(
+                      '%s not found. see build-mark.sh and gfm_reader.py'
+                      % _LIBCMARK
+                      )
 
 # Use ctypes to access the functions in libcmark-gfm
 F_cmark_parser_new = cmark.cmark_parser_new
@@ -47,15 +50,23 @@ F_cmark_parser_new.argtypes = (ctypes.c_int,)
 
 F_cmark_parser_feed = cmark.cmark_parser_feed
 F_cmark_parser_feed.restype = None
-F_cmark_parser_feed.argtypes = (ctypes.c_void_p, ctypes.c_char_p, ctypes.c_size_t)
+F_cmark_parser_feed.argtypes = (
+                                ctypes.c_void_p,
+                                ctypes.c_char_p,
+                                ctypes.c_size_t
+                                )
 
 F_cmark_parser_finish = cmark.cmark_parser_finish
 F_cmark_parser_finish.restype = ctypes.c_void_p
 F_cmark_parser_finish.argtypes = (ctypes.c_void_p,)
 
-F_cmark_parser_attach_syntax_extension = cmark.cmark_parser_attach_syntax_extension
+F_cmark_parser_attach_syntax_extension = \
+        cmark.cmark_parser_attach_syntax_extension
 F_cmark_parser_attach_syntax_extension.restype = ctypes.c_int
-F_cmark_parser_attach_syntax_extension.argtypes = (ctypes.c_void_p, ctypes.c_void_p)
+F_cmark_parser_attach_syntax_extension.argtypes = (
+                                                    ctypes.c_void_p,
+                                                    ctypes.c_void_p
+                                                    )
 
 F_cmark_parser_get_syntax_extensions = cmark.cmark_parser_get_syntax_extensions
 F_cmark_parser_get_syntax_extensions.restype = ctypes.c_void_p
@@ -81,13 +92,13 @@ F_cmark_render_html.argtypes = (ctypes.c_void_p, ctypes.c_int, ctypes.c_void_p)
 # Set up the libcmark-gfm library and its extensions
 F_register = cmark_ext.core_extensions_ensure_registered
 F_register.restype = None
-F_register.argtypes = ( )
+F_register.argtypes = ()
 F_register()
 
-### technically, maybe install an atexit() to release the plugins
+# technically, maybe install an atexit() to release the plugins
 
 # Options for the GFM rendering call
-### this could be moved into SETTINGS or somesuch, but meh. not needed now.
+# this could be moved into SETTINGS or somesuch, but meh. not needed now.
 OPTS = 0
 
 # The GFM extensions that we want to use
@@ -100,115 +111,118 @@ EXTENSIONS = (
 
 
 class GFMReader(pelican.readers.BaseReader):
-	enabled = True
-	"""GFM-flavored Reader for the Pelican system.
+    enabled = True
+    """GFM-flavored Reader for the Pelican system.
 
-	Pelican looks for all subclasses of BaseReader, and automatically
-	registers them for the file extensions listed below. Thus, nothing
-	further is required by users of this Reader.
-	"""
+    Pelican looks for all subclasses of BaseReader, and automatically
+    registers them for the file extensions listed below. Thus, nothing
+    further is required by users of this Reader.
+    """
 
-	# NOTE: the builtin MarkdownReader must be disabled. Otherwise, it will be
-	#       non-deterministic which Reader will be used for these files.
-	file_extensions = ['md', 'markdown', 'mkd', 'mdown']
+    # NOTE: the builtin MarkdownReader must be disabled. Otherwise, it will be
+    #       non-deterministic which Reader will be used for these files.
+    file_extensions = ['md', 'markdown', 'mkd', 'mdown']
 
-	# Metadata is specified as a single, colon-separated line, such as:
-	#
-	# Title: this is the title
-	#
-	# Note: name starts in column 0, no whitespace before colon, will be
-	#       made lower-case, and value will be stripped
-	#
-	RE_METADATA = re.compile('^([A-za-z]+): (.*)$')
+    # Metadata is specified as a single, colon-separated line, such as:
+    #
+    # Title: this is the title
+    #
+    # Note: name starts in column 0, no whitespace before colon, will be
+    #       made lower-case, and value will be stripped
+    #
+    RE_METADATA = re.compile('^([A-za-z]+): (.*)$')
 
-	def read(self, source_path):
-            # Prepare the "slug", which is the target file name. It will be the
-            # same as the source file, minus the leading ".../content/(articles|pages)"
-            # and with the extension removed (Pelican will add .html)
-            relpath = os.path.relpath(source_path, self.settings['PATH'])
-            parts = relpath.split(os.sep)
-            parts[-1] = os.path.splitext(parts[-1])[0]  # split off ext, keep base
-            slug = os.sep.join(parts[1:])
+    def read(self, source_path):
+        # Prepare the "slug", which is the target file name.
+        # It will be the same as the source file, minus the leading
+        # ".../content/(articles|pages)"
+        # and with the extension removed (Pelican will add .html)
+        relpath = os.path.relpath(source_path, self.settings['PATH'])
+        parts = relpath.split(os.sep)
 
-            metadata = {
-                'slug': slug,
-            }
+        # split off ext, keep base
+        parts[-1] = os.path.splitext(parts[-1])[0]
+        slug = os.sep.join(parts[1:])
 
+        metadata = {
+            'slug': slug,
+        }
 
-            
-            # Fetch the source content, with a few appropriate tweaks
-            with pelican.utils.pelican_open(source_path) as text:
+        # Fetch the source content, with a few appropriate tweaks
+        with pelican.utils.pelican_open(source_path) as text:
 
-                # Extract the metadata from the header of the text
-                lines = text.splitlines()
-                for i in range(len(lines)):
-                    line = lines[i]
-                    match = GFMReader.RE_METADATA.match(line)
-                    if match:
-                        name = match.group(1).strip().lower()
-                        if name != 'slug':
-                            value = match.group(2).strip()
-                            if name == 'date':
-                               value = pelican.utils.get_date(value)
-                        metadata[name] = value
-                        #if name != 'title':
-                        #  print 'META:', name, value
-                    elif not line.strip():
-                        # blank line
-                        continue
-                    else:
-                        # reached actual content
-                        break
+            # Extract the metadata from the header of the text
+            lines = text.splitlines()
+            for i in range(len(lines)):
+                line = lines[i]
+                match = GFMReader.RE_METADATA.match(line)
+                if match:
+                    name = match.group(1).strip().lower()
+                    if name != 'slug':
+                        value = match.group(2).strip()
+                        if name == 'date':
+                            value = pelican.utils.get_date(value)
+                    metadata[name] = value
+                    # if name != 'title':
+                    #  print 'META:', name, value
+                elif not line.strip():
+                    # blank line
+                    continue
+                else:
+                    # reached actual content
+                    break
 
-                # Reassemble content, minus the metadata
-                text = '\n'.join(lines[i:])
+            # Reassemble content, minus the metadata
+            text = '\n'.join(lines[i:])
 
-                # Render the markdown into HTML
+            # Render the markdown into HTML
 #                if sys.version_info >= (3, 0):
 #                    text = text.encode('utf-8')
 #                    content = self.render(text).decode('utf-8')
 #                else:
-                content = self.render(text)
+            content = self.render(text)
 
-            # Redo the slug for articles.
-            if parts[0] == 'articles' and 'title' in metadata:
-                metadata['slug'] = pelican.utils.slugify(
-                                    metadata['title'],
-                                    self.settings.get('SLUG_SUBSTITUTIONS', ()))
+        # Redo the slug for articles.
+        if parts[0] == 'articles' and 'title' in metadata:
+            metadata['slug'] = pelican.utils.slugify(
+                                metadata['title'],
+                                self.settings.get(
+                                 'SLUG_SUBSTITUTIONS',
+                                 ()
+                                 )
+                                )
 
-            return content, metadata
+        return content, metadata
 
-	def render(self, text):
-	  "Use cmark-gfm to render the Markdown into an HTML fragment."
+    def render(self, text):
+        "Use cmark-gfm to render the Markdown into an HTML fragment."
 
-	  parser = F_cmark_parser_new(OPTS)
-	  assert parser
-	  for name in EXTENSIONS:
-		ext = F_cmark_find_syntax_extension(name)
-		assert ext
-		rv = F_cmark_parser_attach_syntax_extension(parser, ext)
-		assert rv
-	  exts = F_cmark_parser_get_syntax_extensions(parser)
-
-	  F_cmark_parser_feed(parser, text, len(text))
-	  doc = F_cmark_parser_finish(parser)
-	  assert doc
-
-	  output = F_cmark_render_html(doc, OPTS, exts)
-
-	  F_cmark_parser_free(parser)
-	  F_cmark_node_free(doc)
-
-	  return output
+        parser = F_cmark_parser_new(OPTS)
+        assert parser
+        for name in EXTENSIONS:
+            ext = F_cmark_find_syntax_extension(name)
+            assert ext
+            rv = F_cmark_parser_attach_syntax_extension(parser, ext)
+            assert rv
+        exts = F_cmark_parser_get_syntax_extensions(parser)
+        F_cmark_parser_feed(parser, text, len(text))
+        doc = F_cmark_parser_finish(parser)
+        assert doc
+        output = F_cmark_render_html(doc, OPTS, exts)
+        F_cmark_parser_free(parser)
+        F_cmark_node_free(doc)
+        return output
 
 
 def add_readers(readers):
     if str(gfmSetup.test_configuration()) == "0":
-	readers.reader_classes['md'] = GFMReader
+        readers.reader_classes['md'] = GFMReader
     else:
-        # Optionally we can have the gfmSetup script run here as root and configure the system
-        # Probably just easier to run it independently tho.
-        sys.exit(1)
+        raise Exception('GFM plugin cannot find the required libcmark files
+                        Please run gfmSetup.configure() to build and configure
+                        the appropriate libcmark files'
+                        )
+
 
 def register():
-	pelican.signals.readers_init.connect(add_readers)
+    pelican.signals.readers_init.connect(add_readers)
--- pelican-gfm/gfmSetup.py
+++ pelican-gfm/gfmSetup.py
@@ -3,78 +3,107 @@
 import sys
 import os
 import subprocess
+import gfmVars
 
-# This gets used by the gfm plugin as well as the check_configure function
-LIBCMARKLOCATION="/usr/lib/x86_64-linux-gnu"
 
-ARCHIVES="https://github.com/github/cmark-gfm/archive"
-VERSION="0.28.3.gfm.12"
-LOCAL="cmark-gfm.$VERSION.orig.tar.gz"
-WORKSPACE='/tmp/build-cmark'
+# Eventually, equivalents for
+# other operating systems / package
+# managers could be written for this
 
 def dpkg_installed(package):
     """ Uses Dpkg to determine whether or not a package is installed
     requires: <package name>"""
     t1 = subprocess.Popen(["dpkg", "-l"], stdout=subprocess.PIPE)
-    t2 = subprocess.Popen(["grep", "-q", package], stdout=subprocess.PIPE, stdin=t1.stdout)
+    t2 = subprocess.Popen([
+                            "grep",
+                            "-q",
+                            package
+                          ], stdout=subprocess.PIPE, stdin=t1.stdout)
     data = t2.communicate()
     ec = t2.wait()
     return ec
 
+
 def dpkg_packages_installed():
     """ Checking to see if the appropriate packages are installed"""
     installed = ["cmake", "make", "wget"]
-    removed = ["libcmark-gfm-dev", "libcmark-gfm-extensions-dev", "libcmark-gfm0", "libcmark-gfm-extensions0"]
+    removed = [
+                "libcmark-gfm-dev",
+                "libcmark-gfm-extensions-dev",
+                "libcmark-gfm0",
+                "libcmark-gfm-extensions0"
+              ]
+
     need_to_be_removed = [package for package in removed
                           if dpkg_installed(package) == 0
                           ]
 
-    need_to_be_installed = [package for package in installed
-                          if dpkg_installed(package) == 1
-                          ]
-
+    need_to_be_installed = [
+                            package for package in installed
+                            if dpkg_installed(package) == 1
+                           ]
 
     if len(need_to_be_removed) is not 0:
         raise Exception("Found the following conflicting packages\
  which should be removed:\
  need_to_be_installed")
         rem = False
-    else: rem = True
-    
+    else:
+        rem = True
+
     if len(need_to_be_installed) is not 0:
         raise Exception("Could not find the following required\
 packages: need_to_be_installed")
         ins = False
-    else: ins = True
+    else:
+        ins = True
 
     if rem is True and ins is True:
         print("Packages seem in order.")
         return True
 
+
 def cleanUp():
     subprocess.call(["rm", "-rf", WORKSPACE])
 
-def setup ():
 
+def setup():
     dpkg_packages_installed()
 
     if test_configuration() == 1:
         print("System appears to be configured")
-        
-
     # Configure the environment if it's not already configured
     if not os.path.isdir(WORKSPACE):
         os.mkdir(WORKSPACE)
 
-    subprocess.call(["wget", "--quiet", ARCHIVES + "/" + VERSION + ".tar.gz", WORKSPACE, "-P", WORKSPACE])
-    subprocess.call(['tar', 'zxf', WORKSPACE + "/" + VERSION + ".tar.gz", "-C", WORKSPACE])
+    subprocess.call([
+                     "wget",
+                     "--quiet",
+                     ARCHIVES + "/" + VERSION + ".tar.gz",
+                     WORKSPACE,
+                     "-P",
+                     WORKSPACE
+                     ])
+    subprocess.call([
+                     'tar',
+                     'zxf',
+                     WORKSPACE + "/" + VERSION + ".tar.gz",
+                     "-C",
+                     WORKSPACE
+                     ]
+                    )
 
     BUILDSPACE = WORKSPACE + "/" + "cmark-gfm-" + VERSION + "/build"
 
     if not os.path.isdir(BUILDSPACE):
         os.mkdir(BUILDSPACE)
 
-    thing1 = subprocess.Popen(["cmake", "-DCMARK_TESTS=OFF", "-DCMARK_STATIC=OFF", ".."], cwd=BUILDSPACE)
+    thing1 = subprocess.Popen([
+                                "cmake",
+                                "-DCMARK_TESTS=OFF",
+                                "-DCMARK_STATIC=OFF",
+                                ".."
+                              ], cwd=BUILDSPACE)
     thing1.wait()
 
     thing2 = subprocess.Popen(["make"], cwd=BUILDSPACE)
@@ -82,21 +111,35 @@ def setup ():
 
     # Move the libcmark.so artifacts in place
     print "Moving files"
-    subprocess.call(["mv", BUILDSPACE + "/src/libcmark-gfm.so." + VERSION, LIBCMARKLOCATION + "libcmark-gfm.so"])
-    subprocess.call(["mv", BUILDSPACE + "/extensions/libcmark-gfmextensions.so." + VERSION, LIBCMARKLOCATION + "libcmark-gfmextensions.so"])
+    subprocess.call([
+                     "mv",
+                     BUILDSPACE + "/src/libcmark-gfm.so." + VERSION,
+                     LIBCMARKLOCATION + "libcmark-gfm.so"
+                    ]
+                    )
+    subprocess.call([
+                     "mv",
+                     BUILDSPACE + "/extensions/libcmark-gfmextensions.so." + VERSION,
+                     LIBCMARKLOCATION + "libcmark-gfmextensions.so"
+                    ]
+                    )
+
 
 def test_configuration():
     """ Tests to ensure that the files that the plugin needs are in place. """
     CMARKPATH = LIBCMARKLOCATION + "/libcmark-gfm.so." + VERSION
-    if os.path.isfile(LIBCMARKLOCATION + "/libcmark-gfm.so") and os.path.isfile(LIBCMARKLOCATION + "/libcmark-gfmextensions.so"):
+    if os.path.isfile(LIBCMARKLOCATION + "/libcmark-gfm.so") and
+    os.path.isfile(LIBCMARKLOCATION + "/libcmark-gfmextensions.so"):
         return 0
     else:
         return 1
+
 
 def configure():
         print("Checking out the configuration")
         setup()
         cleanUp()
+
 
 if __name__ == "__main__":
     configure()
